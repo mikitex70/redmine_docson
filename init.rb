ff7b50ff2ae7d6fdc7bd84a8ae3bfdc8616a99f7
Michele Tessaro <michele.tessaro@email.it>
2016-07-07 20:17:57 +0200

    chg: dev: some small changes
    * fixed plugin description
    * removed log statements used while in develop

--- init.rb
+++ init.rb
@@ -1,14 +1,10 @@
-Rails.logger.info 'Avvio del mio plugin'
-
 Redmine::Plugin.register :redmine_docson do
   name 'Redmine Docson plugin'
   author 'Michele Tessaro'
-  description 'Wiki macro plugin for inserting docson widgets in Wiki pages'
+  description 'Wiki macro plugin for inserting docson widgets in Wiki pages and Issues'
   version '0.0.1'
-  #url 'http://example.com/path/to/plugin'
+  url 'https://github.com/mikitex70/redmine_docson'
   #author_url 'http://example.com/about'
-
-  Rails.logger.info 'Mio plugin registrato'
 
   Redmine::WikiFormatting::Macros.register do
     desc <<EOF
@@ -16,11 +12,10 @@ Redmine::Plugin.register :redmine_docson do
       
       {{docson(path/to/json/schema)}}
 
-      JSON schema path must be relative to redmine and without ".json" extension.
+      JSON schema path must be relative to redmine public folder.
 EOF
 
     macro :docson do |obj, args|
-      Rails.logger.info 'Macro chiamata'
       content = "<script src=\"/docson/widget.js\" data-schema=\"/jsonSchemas/#{args[0]}\"></script>"
 
       return "#{CGI::unescapeHTML(content)}".html_safe
